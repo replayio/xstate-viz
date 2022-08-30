8d479dffade73952957c9ab39921fa086f4eb94d
Jaril <jarilvalenciano@gmail.com>
2022-08-30 13:02:17 -0700

    Add Replay

---
+++ README_REPLAY.md
@@ -0,0 +1,12 @@
+Links
+
+- [Team invitation](https://app.replay.io/team/invitation?code=4900e29e-5108-4059-952f-8916cb328946)
+- [Link to team](https://app.replay.io/team/dzoxYWY1MzU1ZS03MzQ0LTQyOTMtYmY2Ny1lM2ZjMzFhNGUyOWU=/runs)
+
+Running the tests locally
+
+1. ?
+
+Running the tests locally with Replay
+
+1. ?
--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -1,3 +1,5 @@
+const cypressReplay = require("@replayio/cypress");
+
 /// <reference types="cypress" />
 // ***********************************************************
 // This example plugins/index.js can be used to load plugins
@@ -17,6 +19,9 @@
  */
 // eslint-disable-next-line no-unused-vars
 module.exports = (on, config) => {
+  cypressReplay.default(on, config);
+
+  return config;
   // `on` is used to hook into various events Cypress emits
   // `config` is the resolved Cypress config
 }
